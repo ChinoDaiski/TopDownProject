5ff289a7e1b848dee34cd5a110115671d5892a8e
ChinoDaiski <callmeson1@naver.com>
2025-02-18 20:32:40 +0900

    feat: 게임 리소스 및 플레이어 컨트롤러 추가
    - 배경, 캐릭터 이미지 리소스 추가
    - 사운드 효과 및 배경 음악 파일 추가
    - 플레이어 이동 및 마우스 위치에 따라 바라보는 방향 바꾸는 기능 구현

---
+++ Assets/Scripts/Entity/BaseController.cs
@@ -0,0 +1,104 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class BaseController : MonoBehaviour
+{
+    protected Rigidbody2D m_rigidbody2D;
+
+    [SerializeField] private SpriteRenderer characterRenderer;
+    [SerializeField] private Transform weaponPivot;
+
+    // 이동 방향
+    protected Vector2 movementDirection = Vector2.zero;
+    public Vector2 MovementDirection { get { return movementDirection; } }
+
+
+    // 바라보는 방향
+    protected Vector2 lookDirection = Vector2.zero;
+    public Vector2 LookDirection { get { return lookDirection; } }
+
+
+    private Vector2 knockBack = Vector2.zero;
+    private float knockbackDuration = 0f;
+
+    protected virtual void Awake()
+    {
+        m_rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    protected abstract void Start();
+
+    protected virtual void Update()
+    {
+        // 입력, 이동에 관한 데이터 처리
+        HandleAction();
+
+        // 바라보는 방향에 따른 회전 처리.
+        Rotate(lookDirection);
+    }
+
+    protected virtual void FixedUpdate()
+    {
+        // 움직임
+        Movement(movementDirection);
+
+        // 넉백 효과가 있다면, 적용 시간을 감소
+        if(knockbackDuration > 0f)
+        {
+            knockbackDuration -= Time.deltaTime;
+        }
+    }
+
+    protected abstract void HandleAction();
+
+    private void Movement(Vector2 direction)
+    {
+        direction = direction * 5;
+
+        // 만약 넉백이 적용되어야한다면
+        if(knockbackDuration > 0f)
+        {
+            // 기존 이동방향의 힘을 줄이고
+            direction *= 0.2f;
+
+            // 넉백을 적용시킨다.
+            direction += knockBack;
+        }
+
+        // direction을 강체의 속도에 적용한다.
+        m_rigidbody2D.velocity = direction;
+    }
+
+    // 컨트롤 되는 모든 객체를 특정 방향으로 회전시키는 함수
+    // 방향에 따라 좌/우를 판별하고, 객체를 뒤집는다.
+    // 만약 객체에 무기가 있다면, 해당 무기 방향을 인자로 받은 방향에 맞는 각도로 대입한다.
+    private void Rotate(Vector2 direction)
+    {
+        // 인자로 받은 방향의 각도를 구한다. 각도는 radian으로 되어 있으므로 degree로 바꿔주기 위해 Rad2Deg를 곱해 각으로 바꿔준다.
+        float rotZ = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+
+        // Atan2로 구한 각도는 1,4 사분면에서 -90 ~ 90의 값을 가진다.
+        // 2,3 사분면에선 절댓값으로 따지면 90 ~ 180의 값을 가지므로 90f 이상이라면 왼쪽이라고 볼 수 있다.
+        bool isLeft = Mathf.Abs(rotZ) > 90f;
+
+        // 기본 이미지로 사용하는 sprite는 오른쪽을 바라보고 있으므로, flipX에 true값을 넣으면 x축 기준으로 좌우로 뒤집는다.
+        // y축을 기준으로 180도 회전하는 것과 같다. 
+        characterRenderer.flipX = isLeft;
+
+        // 무기가 존재한다면
+        if(weaponPivot)
+        {
+            weaponPivot.rotation = Quaternion.Euler(0f, 0f, rotZ);
+        }
+    }
+
+    // 넉백 적용 함수
+    // 인자로 받은 위치에서 해당 객체에게 넉백을 가한 것.
+    // 방향은 상대로부터 나를 향한 것이고, 나를 기준으론 내가 가고자 하는 방향의 반대로 밀려나는 힘이다.
+    public void ApplyKnockBack(Transform other, float power, float duration)
+    {
+        knockbackDuration = duration;
+        knockBack = (transform.position - other.position).normalized * power;
+    }
+}
---
+++ Assets/Scripts/Entity/PlayerController.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : BaseController
+{
+    private Camera m_Camera;
+    protected override void Start()
+    {
+        m_Camera = Camera.main;
+    }
+
+    protected override void Update()
+    {
+        base.Update();
+    }
+
+    protected override void HandleAction()
+    {
+        // 1. [ 부드러운 값 반환 (Smooth Input) ]
+        // Input.GetAxis는 0에서 입력 방향으로 부드럽게 변화하는 값을 반환
+        // 예를 들어, 키를 누르면 즉시 1이 아니라 0 → 0.1 → 0.2 → … → 1처럼 천천히 증가하며
+        // 반대로 키를 떼면 1 → 0.9 → 0.8 → ... → 0처럼 서서히 감소
+        // 이 때문에 캐릭터가 부드럽게 움직이고, 조작이 자연스러움
+
+        // 2. [ -1 ~1 사이의 값 반환 ]
+        // Input.GetAxis("Horizontal")는 왼쪽(-1) ~오른쪽(1) 범위의 값을 반환
+        // Input.GetAxis("Vertical")은 아래(-1) ~위(1) 범위의 값을 반환
+        // 중립(키를 누르지 않음)일 때는 0
+
+        // 3. [ Unity의 Input Manager 설정 사용 ]
+        // Edit → Project Settings → Input Manager에서 직접 축을 설정
+        // 기본적으로 Horizontal은 A / D, LeftArrow / RightArrow 키를 사용
+        // Vertical은 W / S, UpArrow / DownArrow 키를 사용
+
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        // 입력값으로 방향 벡터를 구함
+        movementDirection = new Vector2 (horizontal, vertical).normalized;
+
+        Vector2 mousePosition = Input.mousePosition;
+        Vector2 worldPosition = m_Camera.ScreenToWorldPoint(mousePosition);     // 카메라의 near에 위치한 평면에 있는 마우스 좌표를 월드 좌표로 변환
+        lookDirection = worldPosition - (Vector2)transform.position;            // 카메라의 near 좌표에서 플레이어가 위치한 곳을 바라보는 방향을 구함.
+                                                                                // 2d 게임에선 캐릭터도 near 평면에 위치함
+
+        // 만약 마우스와 캐릭터의 위치 차이가 1칸 미만이라면
+        if (lookDirection.magnitude < 0.9f)
+        {
+            // 방향 값을 주지 않는다.
+            lookDirection = Vector2.zero;
+        }
+        // 마우스와 캐릭터의 위치 차이가 1칸 이상이라면
+        else
+        {
+            // 해당 방향의 방향 벡터를 구한다.
+            lookDirection = lookDirection.normalized;
+        }
+    }
+
+
+}
